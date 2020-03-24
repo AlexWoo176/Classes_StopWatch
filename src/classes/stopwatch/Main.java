fadb6790433d1adf7b61680c76fcd855801bc5a1
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-03-24 18:12:31 +0700

    fixes

---
+++ src/classes/stopwatch/Main.java
@@ -0,0 +1,29 @@
+package classes.stopwatch;
+
+public class Main {
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start();
+        int[] listArrayNumber = new int[100000];
+        for (int i = 0; i < 100000 ; i++) {
+            listArrayNumber[i] = (int) Math.floor(Math.random()*10000);
+        }
+        int temp = 0;
+        for (int i = 0; i < listArrayNumber.length - 1 ; i++) {
+            for (int j = 0; j < listArrayNumber.length - 1 - i; j++) {
+                if (listArrayNumber[j] > listArrayNumber[j+1]) {
+                    temp += listArrayNumber[j];
+                    listArrayNumber[j] = listArrayNumber[j+1];
+                    listArrayNumber[j+1] = temp;
+                }
+            }
+        }
+
+        stopWatch.stop();
+
+        System.out.println("Start Time: \n" + stopWatch.startTime + " milisecond");
+        System.out.println("End Time: \n" + stopWatch.endTime + " milisecond");
+        System.out.println("Time is to operate: \n" + stopWatch.getElapsedTime() + " milisecond");
+    }
+}
--- src/classes/stopwatch/StopWatch.java
+++ src/classes/stopwatch/StopWatch.java
@@ -25,30 +25,4 @@ public class StopWatch {
     public long getElapsedTime() {
         return endTime - startTime;
     }
-
-    public static void main(String[] args) {
-        StopWatch stopWatch = new StopWatch();
-
-        stopWatch.start();
-        int[] listArrayNumber = new int[100000];
-        for (int i = 0; i < 100000 ; i++) {
-            listArrayNumber[i] = (int) Math.floor(Math.random()*10000);
-        }
-        int temp = 0;
-        for (int i = 0; i < listArrayNumber.length - 1 ; i++) {
-            for (int j = 0; j < listArrayNumber.length - 1 - i; j++) {
-                if (listArrayNumber[j] > listArrayNumber[j+1]) {
-                    temp += listArrayNumber[j];
-                    listArrayNumber[j] = listArrayNumber[j+1];
-                    listArrayNumber[j+1] = temp;
-                }
-            }
-        }
-
-        stopWatch.stop();
-
-        System.out.println("Start Time: \n" + stopWatch.startTime + " milisecond");
-        System.out.println("End Time: \n" + stopWatch.endTime + " milisecond");
-        System.out.println("Time is to operate: \n" + stopWatch.getElapsedTime() + " milisecond");
-    }
 }
